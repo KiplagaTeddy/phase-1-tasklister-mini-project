c4deca585146aa7482369cd347ea6d0066f221b8
Teddy <teddykipchumba17@gmail.com>
2024-04-01 23:11:02 +0300

    Mini Project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+const newForm = document.querySelector('form');
+newForm.addEventListener('submit', (Task) => {
+  Task.preventDefault();
+  const newToDo = Task.target.querySelector('#new-task-description').value
+  buildToDo(newToDo);
+  newForm.reset();
+})
+})
+
+function buildToDo(newToDo){
+  const taskLi = document.createElement('li');
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = 'x';
+  taskLi.textContent = newToDo;
+  taskLi.appendChild(deleteButton);
+  document.querySelector('#tasks').appendChild(taskLi);
+  deleteButton.addEventListener('click', handleDelete);
+  
+  function handleDelete(Task){
+    Task.target.parentNode.remove();
+  }
+}
